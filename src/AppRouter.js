5dc1cfc6fbfa8309875db188b56dd59199e120d1
fsociety <americanrock988@gmail.com>
2025-07-28 10:20:38 -0700

    fixed forms

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,6 +5,42 @@ import AdminPanel from './AdminPanel';
 import AdminRoute from './AdminRoute';
 import Login from './Login'; // Import the new Login component
 
+// Thanks component that mimics the static HTML page
+const Thanks = () => {
+  return (
+    <div style={{
+      fontFamily: 'Arial, sans-serif',
+      textAlign: 'center',
+      padding: '50px 20px'
+    }}>
+      <div style={{
+        maxWidth: '600px',
+        margin: '0 auto'
+      }}>
+        <h1>Thank You!</h1>
+        <p style={{
+          fontSize: '1.2em',
+          lineHeight: '1.6',
+          color: '#34495e'
+        }}>
+          Your message has been sent successfully. We'll get back to you soon!
+        </p>
+        <p>
+          <a 
+            href="/" 
+            style={{
+              color: '#3498db',
+              textDecoration: 'none'
+            }}
+          >
+            ← Back to Home
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const AppRouter = () => {
   return (
     <Router>
@@ -19,6 +55,7 @@ const AppRouter = () => {
             </AdminRoute>
           }
         />
+        <Route path="/thanks/" element={<Thanks />} />
       </Routes>
     </Router>
   );
